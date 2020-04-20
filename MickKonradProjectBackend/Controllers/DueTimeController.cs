252fec2695f717d4ea9676ce52ef150b432374fc
Grizzly232 <Grizzly232@github.com>
2020-04-20 21:33:09 +0100

    stored values in local collection because SQLServer wouldn't work

--- MickKonradProjectBackend/Controllers/DueTimeController.cs
+++ MickKonradProjectBackend/Controllers/DueTimeController.cs
@@ -1,8 +1,10 @@
 ﻿using MickKonradProjectBackend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,12 +18,28 @@ namespace MickKonradProjectBackend.Controllers
     {
         private static readonly HttpClient client = new HttpClient();
         private readonly DueTimeContext _context;
+        List<DueTime> list = new List<DueTime>();
 
         public DueTimeController(DueTimeContext context)
         {
             _context = context;
-
-            if (!_context.dueTime.Any())
+            
+            list.Add(new DueTime
+            { Id = 1, StopId = 2335, TimeDue = 10, Route = "27" });
+            list.Add(new DueTime
+            { Id = 2, StopId = 2336, TimeDue = 15, Route = "77A" });
+            list.Add(new DueTime
+            { Id = 3, StopId = 2335, TimeDue = 18, Route = "27" });
+            list.Add(new DueTime
+            { Id = 4, StopId = 251, TimeDue = 3, Route = "9" });
+            list.Add(new DueTime
+            { Id = 5, StopId = 251, TimeDue = 5, Route = "41" });
+            list.Add(new DueTime
+            { Id = 6, StopId = 251, TimeDue = 16, Route = "150" });
+            list.Add(new DueTime
+            { Id = 7, StopId = 251, TimeDue = 21, Route = "9" });
+            _context.SaveChanges();
+            /*if (!_context.dueTime.Any())
             {
                 _context.dueTime.Add(new DueTime
                 { Id = 1, StopId = 2335, TimeDue = 10, Route = "27" });
@@ -38,15 +56,16 @@ namespace MickKonradProjectBackend.Controllers
                 _context.dueTime.Add(new DueTime
                 { Id = 7, StopId = 251, TimeDue = 21, Route = "9" });
                 _context.SaveChanges();
-            }
+            }*/
         }
 
         [HttpGet]
         [Route("stop/{stopId}")]
         [Produces("application/json")]
-        public IQueryable GetByStop(int stopId)
+        public IEnumerable<DueTime> GetByStop(int stopId)
         {
-            return _context.dueTime.Where<DueTime>(record => record.StopId == stopId);
+            //return _context.dueTime.Where<DueTime>(record => record.StopId == stopId);
+            return list.Where<DueTime>(record => record.StopId == stopId);
         }
     }
 }
